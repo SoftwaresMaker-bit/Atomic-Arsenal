25d6e38095ad8bd512c3525e8b9751782ed25405
SoftwaresMaker-bit <A01712819@tec.mx>
2024-11-10 18:17:26 -0600

    Update Weapon.h

--- Weapon.h
+++ Weapon.h
@@ -5,14 +5,14 @@ class Weapon {
 
 private:
 
-	-char name;
-	-int damage;
-	-char chemistry;
+	char name;
+	int damage;
+	char chemistry;
 
 public:
-	+ Weapon(name);
-	+ int Get_damage();
-	+ char Get_chemistry();
+	Weapon(name);
+	int Get_damage();
+	char Get_chemistry();
 
 
 
