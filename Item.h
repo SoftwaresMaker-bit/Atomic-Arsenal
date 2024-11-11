874856069d160df3c9203896064987443625d9b7
SoftwaresMaker-bit <A01712819@tec.mx>
2024-11-10 18:18:44 -0600

    Update Item.h

--- Item.h
+++ Item.h
@@ -3,12 +3,12 @@ Item.h
 class Item{
 
 private:
-	-char name;
-	-int quantity;
-	-char player;
+	char name;
+	int quantity;
+	char player;
 
 public:
-	+item(name);
-	+ void use(player);
+	item(name);
+	void use(player);
 
 }
