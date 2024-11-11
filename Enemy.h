1255cb8d5faa48159b80ffe0470efc92176ce10f
SoftwaresMaker-bit <A01712819@tec.mx>
2024-11-10 18:21:14 -0600

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -3,20 +3,16 @@ Enemy.h
 class Enemy {
 
 private: 
-	-char name;
-	- int hp; // Health points, enemies life 
-	- int dp; // damage points, enemies damage to the player 
-	- char chemistry;
-	- int damage; // damage receive from the player, used in function Hurted
-	- char player;
-	- char damage_chemistry; 
+	char name;
+	int hp; // Health points, enemies life 
+	char chemistry;
+	int damage_received; // damage receive from the player, used in function Hurted
+	char damage_received_chemistry; 
 
 public: 
-
-	+Enemy(name);
-	+ void Hurted(damage);
-	+ void Attack(DP,player);
-	+ int Dmultiplier(damage,damage_chemistry)
+	Enemy(name);
+	void Hurted(damage);
+	int Dmultiplier(damage_received,damage_received_chemistry)
 
 
 
