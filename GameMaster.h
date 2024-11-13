95293f9c997e5a47691171f223c217574663751e
SoftwaresMaker-bit <A01712819@tec.mx>
2024-11-12 19:56:33 -0600

    Update GameMaster.h

--- GameMaster.h
+++ GameMaster.h
@@ -2,13 +2,13 @@ GameMaster.h
 
 class GameMaster{
 private:
-	char player;
-	char enemy;
+	player:Player
+	enemy:Enemy
 	int turn; // Variable for the turn of either the player or the enemy. 0 =player, 1 =enemy
 
 public:
 	GameMaster(char,char,int);
 	void Start_Battle();
-	void Manage_turns(turn);
+	void Manage_turns(int);
 	void End_Battle();
 }
