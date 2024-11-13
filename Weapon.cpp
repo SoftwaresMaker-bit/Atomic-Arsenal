f2b3dae74e2fa3cf59e0cb1b04fa33729c0a5d3c
SoftwaresMaker-bit <A01712819@tec.mx>
2024-11-12 19:41:44 -0600

    Update Weapon.cpp

--- Weapon.cpp
+++ Weapon.cpp
@@ -3,15 +3,21 @@ Weapon.cpp
 #include <iostream>
 #include "Weapon.h"
 
-Weapon::Weapon(char _name){
-
+Weapon::Weapon(char _name,int _DP, char _chemistry){
+	
 	name = _name;
+	DP = _DP;
+	chemistry = _chemistry;
 }
 
-int Weapon::Get_damage(){
-
-	return damage;
+int Weapon::Get_name(){
+	return name;
 }
+
+int Weapon::Get_DP(){
+	return DP;
+}
+
 
 char Weapon::Get_chemistry(){
 
