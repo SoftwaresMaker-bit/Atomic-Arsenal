d772eaf409bf6bd68f91023c2f8ad00de8f10613
SoftwaresMaker-bit <A01712819@tec.mx>
2024-11-12 19:49:19 -0600

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -3,22 +3,28 @@ Player.cpp
 #include <iostream>
 #include "Player.h"
 
-Player::Player(char _name){
-
-	name = _name
+Player::Player(char _name, int _hp, int _dp){
+	name = _name;
+	hp = _hp;
+	dp = _dp;
 }
 
-void Player:: Attack(){
-
-
-}
-
-void Player:: use_item(){
-
+void Player:: Attack(int dp){
 
 }
 
-void Player:: hurted(int _damage){
+void Player:: hurted(int damage){
+	hp = hp - damage;
+}
 
-	HP = HP - _damage
+int Player::Get_HP(){
+	return hp;
+}
+
+void Player::Set_DP(int _dp){
+	dp = _dp;
+}
+
+int Player::Get_DP(){
+	return dp;
 }
