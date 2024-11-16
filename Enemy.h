7885979cea6d23e4f71525bb8b594e0eb40139f4
SoftwaresMaker-bit <A01712819@tec.mx>
2024-11-16 13:53:04 -0600

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -8,7 +8,7 @@ protected:
 	int dp; // Damage points send to the player class throught the attack function
 	char chemistry;
 public: 
-	Enemy(char,int,int,char);
+	Enemy(char _name,int _hp,int _dp,char _chemistry);
 	void Hurted(int, char); // this parameters are sended by the player
 	void Attack(int);
 	int Get_HP();
