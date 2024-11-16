75f8e755d05624227534810620d645b290a0ad34
SoftwaresMaker-bit <A01712819@tec.mx>
2024-11-16 13:48:30 -0600

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,5 +1,12 @@
 Player.h
+#ifndef PLAYER_H
+#define PLAYER_H
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+	
 class Player{
 
 private:
@@ -9,10 +16,11 @@ private:
 	
 
 public:
-	Player(char,int,int);
+	Player(char name,int hp,int dp);
 	void Attack(int);
 	void Hurted(int); // parameter sended by enemy
 	int Get_HP();
 	void Set_DP(int);
 	int Get_DP();
-}
+};
+#endif
